acaaeb986ca895fd2d8cbf360a7c56bcb61cda61
Alexandre Szymocha <alexandre@szymocha.com>
2019-07-13 18:31:25 +0200

    Move `memoryBody` in another file
    This function will be used by machines other than Slack.

--- src/Machines.fs
+++ src/Machines.fs
@@ -18,49 +18,19 @@
 (******************************************************************************)
 
 [<AutoOpen>]
-module Reward.Machines.Slack.__main__
+module Reward.Machines.__main__
 #nowarn "62"
 #light "off"
 
 open System
-open Aether
-open Aether.Operators
 open Freya.Core
 open Freya.Optics.Http
-open Reward.Config
-open Reward.Machines
-open Reward.Optics
-open Reward.Yolo
 
-let hmac =
-  let bytes = Utf8.bytes Config.slack.signingSecret in
-  new Security.Cryptography.HMACSHA256(bytes)
-
-let verifySlackRequest = freya
-{ let inline ( <|> ) v f = Option.filter f v in
-  let inline ( <!> ) v f = Option.map f v in
-  let now = DateTimeOffset.UtcNow.ToUnixTimeSeconds() in
-  do! memoryBody in
-  let! shash = Freya.Optic.get (Request.header_ "X-Slack-Signature") in
-  let! headers =
-    [ Request.header_ "X-Slack-Request-Timestamp" >-> Option.value_
-    ; Request.body_ >-> String.stream_
-    ] |> List.traverseFreyaA Freya.Optic.get
-      |> Freya.map List.sequenceOptionA
-      |> Freya.map (Option.bind List.toTuple)
-  in
-  do! Freya.Optic.set (Request.body_ >-> Stream.pos_) 0L in
-  return headers
-  <|> ( fun data ->
-          data^.(fst_ >-> Int64.string_)
-          <|> (( - ) now >> ( > ) 10L)
-          |> Option.isSome
-      )
-  <!> ( fun (ts, body) ->
-          let base' = String.Join(':', "v0", ts, body) in
-          let hash = hmac.ComputeHash(Utf8.bytes base') in
-          "v0=" + Bytes.toStringHex hash
-      )
-  |> Option.lift2 ( = ) shash <|> id
-  |> Option.isSome
-}
+let memoryBody =
+  Freya.Optic.map
+    Request.body_
+    <| fun ks ->
+         let copy = new IO.MemoryStream() in
+         ks.CopyTo(copy);
+         copy.Position <- 0L;
+         upcast copy
